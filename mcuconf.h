9694e6172dabb91f5e19f9f4dd83e7cd22cef128
oden42 <81540829+oden42@users.noreply.github.com>
2023-05-15 14:05:12 -0700

    Update mcuconf.h
    Commented out I2C1, and changed to PWM5

--- mcuconf.h
+++ mcuconf.h
@@ -1,19 +1,15 @@
 #pragma once
 
-
 #include_next <mcuconf.h>
-/*
 
-#undef RP_SPI_USE_SPI1
-#define RP_SPI_USE_SPI1 TRUE
-
-*/
+// #undef RP_SPI_USE_SPI1
+// #define RP_SPI_USE_SPI1 TRUE
 
 #undef RP_I2C_USE_I2C0
 #define RP_I2C_USE_I2C0 TRUE
 
-#undef RP_I2C_USE_I2C1
-#define RP_I2C_USE_I2C1 TRUE
+// #undef RP_I2C_USE_I2C1
+// #define RP_I2C_USE_I2C1 TRUE
 
-#undef RP_PWM_USE_PWM0
-#define RP_PWM_USE_PWM0  TRUE
+#undef RP_PWM_USE_PWM5
+#define RP_PWM_USE_PWM5  TRUE
