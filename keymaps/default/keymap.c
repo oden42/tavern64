577483753dfaaf341d0755cbb21020f34bb044a1
oden42 <81540829+oden42@users.noreply.github.com>
2023-05-16 20:34:50 -0700

    Small update

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//	 	COL 0,		1,		2,		3,		4,		5,		6,		7,		8,		9,		10,		11,		12,		13		ROW
 			KC_ESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS, KC_EQL,KC_BSPC,	// 	0
 			KC_TAB, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  , KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,KC_RBRC,KC_BSLS, //	1
-			KC_P1 ,KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT, KC_ENT, //	2
+			TO(1) ,KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT, KC_ENT, //	2
 			KC_P2 ,KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH, KC_UP , KC_DEL, //	3
 			KC_P3 ,KC_LCTL,KC_LGUI,KC_LALT, KC_SPC, 	    KC_SPC, KC_SPC, 	    KC_P4 ,KC_LEFT,KC_DOWN,KC_RGHT 		    //	4
 	  ),
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// 		COL 0,		1,		2,		3,		4,		5,		6,		7,		8,		9,		10,		11,		12,		13		ROW
 			KC_GRV, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11, KC_F12, KC_DEL,	// 	0
 			KC_TAB, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  , KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,KC_RBRC,KC_BSLS, //	1
-			KC_P1 ,KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT, KC_ENT, //	2
+			TO(0) ,KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT, KC_ENT, //	2
 			KC_P2 ,KC_LSFT,QMKSONG1,QMKSONG2,QMKSONG3,QMKSONG4,QMKSONG5,QMKSONG6,QMKSONG7,QMKSONG8,QMKSONG9,QMKSONG10, KC_UP , KC_DEL, //	3
 			KC_P3 ,KC_LCTL,KC_LGUI,KC_LALT, KC_SPC, 	    KC_SPC, KC_SPC, 	    KC_P4 ,KC_LEFT,KC_DOWN,KC_RGHT 		    //	4
 	  ),
@@ -132,7 +132,7 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 	//			Encoder 1 (2,0)						Encoder 2 (3,0)						Encoder 3 (4,0)
     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),	ENCODER_CCW_CW(KC_1, KC_2),			ENCODER_CCW_CW(KC_3, KC_4) },
-    // [_WASD] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [_ONE] =  { ENCODER_CCW_CW(KC_5, KC_6),	ENCODER_CCW_CW(KC_7, KC_8),			ENCODER_CCW_CW(KC_9, KC_0) },
     // [_ARRW] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     // [_RGB]  = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
