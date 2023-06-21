df066f70580cb92a20fae55fb6f912f4a8be8a28
oden42 <81540829+oden42@users.noreply.github.com>
2023-06-21 10:54:25 -0700

    Add files via upload

--- rules.mk
+++ rules.mk
@@ -2,18 +2,18 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite. 					MOVE TO JSON
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+# RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = yes          # Audio output
 AUDIO_DRIVER = pwm_hardware	# Audio driver (required for RP2040)
 ENCODER_MAP_ENABLE = yes	# Allows mapping of encoders per layer		MOVE TO JSON OR REMOVE??
-RGB_MATRIX_ENABLE = no		# Enable per-key RGB lighting matrix
-# RGB_MATRIX_DRIVER = WS2812
+RGB_MATRIX_ENABLE = yes		# Enable per-key RGB lighting matrix
+RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = vendor		# Required for RP2040
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
