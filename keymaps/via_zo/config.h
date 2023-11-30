8a8e69341fe63cb54869e89615e9d77da96baef3
oden42 <81540829+oden42@users.noreply.github.com>
2023-11-29 23:36:39 -0800

    Add Zo Edition

---
+++ keymaps/via_zo/config.h
@@ -0,0 +1,13 @@
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
+#ifdef STARTUP_SONG
+	#undef STARTUP_SONG
+#endif
+#define STARTUP_SONG SONG(MARIO_MUSHROOM)
+#ifdef PRODUCT_ID
+	#undef PRODUCT_ID
+#endif
+#define PRODUCT_ID 0x0A64
+#ifdef PRODUCT
+	#undef PRODUCT
+#endif
+#define PRODUCT "Tavern64 Zo Edition"
